1a2cc879064aa6a34deacaac977d3a999facb6c6
Radoslav Genov <radoslavgenov5@hotmail.com>
2016-06-16 16:39:10 +0300

    update

---
+++ TextFiles/ReplaceSubString/ReplaceSubString.cs
@@ -0,0 +1,26 @@
+﻿namespace ReplaceSubString
+{
+    using System.IO;
+
+    class ReplaceSubString
+    {
+        public static void ReplaceString(string path)
+        {
+            string replaced;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string all = reader.ReadToEnd();
+                replaced = all.Replace("start", "finish");
+            }
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                writer.WriteLine(replaced);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ReplaceString(@"..\..\TextFile1.txt");
+        }
+    }
+}
